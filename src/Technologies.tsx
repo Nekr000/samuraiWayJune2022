c501bb9cfcc0202dfd78a85e26608a6029be1f73
Nekr000 <nekr00ggg@gmail.com>
2022-09-23 03:23:01 +0300

    1

---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>JS</li>
+
+        </div>
+    )
+}
+export default Technologies
